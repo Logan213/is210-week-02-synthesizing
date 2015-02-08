b7489f13ffafc6169c3213ef210de24b4c05f0eb
Logan Thomson <logan.t.music@gmail.com>
2015-02-08 23:36:49 +0000

    This is my commmit for task 3

--- task_03.py
+++ task_03.py
@@ -1,7 +1,8 @@
-CHARLIE = "Chocolate Factory" 
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-j
+"""A Name Association file"""
+
+CHARLIE = "Brown"
 VIOLET = "Gray"
 PATRICIA = "Reichardt"
 LINUS = "van Pelt"
-
-
-
